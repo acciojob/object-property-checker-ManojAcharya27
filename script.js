ecc50adc64639d68c6485c5ebb60309373f5e8eb
Manoj <117893476+ManojAcharya27@users.noreply.github.com>
2023-07-10 15:49:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-	eturn sampleObject.hasOwnProperty(key);
+	return sampleObject.hasOwnProperty(key);
 }
 
 // Do not change the code below
